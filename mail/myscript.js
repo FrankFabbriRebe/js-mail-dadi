fd997b100d9a12bca31e014e410f71491739d836
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-22 17:29:01 +0100

    add: var emailTrovata feat: for cicle to check if the email exist add: print result

--- mail/myscript.js
+++ mail/myscript.js
@@ -1,12 +1,23 @@
-// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-
 // input email user
 const email = prompt("Inserisci la tua email");
 
 // array valid email
 const validEmail = ["franco@gmail.com", "paolo@gmail.com", "marco@gmail.com"];
 
-// create for cicle
-for (let i = 0; i < validEmail.length; i ++)    {
-    console.log(validEmail[i]);
+// create boolean var to ckeck if the email exist or not
+let emailTrovata;
+
+// cicle to check every single elemet of the arry 
+for (let i = 0; i < validEmail.length; i++) {
+    if (email === validEmail[i]) {
+        emailTrovata = true;
+    }
 }
+
+// console.log result
+if (emailTrovata) {
+    console.log("Ok, la tua email è stata trovata!");
+} else {
+    console.log("Errore, la tua email non è stata trovata!");
+}
+
