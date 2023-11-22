470e41a2216f15cb40e4eb6efce64af9daebd1c4
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-22 17:40:16 +0100

    feat: random user number

---
+++ dadi/myscript.js
@@ -0,0 +1,5 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// random user number
+const userNumber = Math.floor(Math.random() * 6) + 1;
+//console.log(userNumber);
