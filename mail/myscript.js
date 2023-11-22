54ec44ae2c2d60e4803ef1cc313e7ffb2143a817
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-22 16:26:40 +0100

    feat: prompt email user

---
+++ mail/myscript.js
@@ -0,0 +1,5 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// input email user
+const email = prompt("Inserisci la tua email");
+
